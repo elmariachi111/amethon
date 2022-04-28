e09d7c191b96b808d3d6ead149bdf6ecbbf8702b
Stefan Adolf <stadolf@gmail.com>
2022-04-28 09:29:01 +0200

    drops need for private keys -> truffle dashboard
    Signed-off-by: Stefan Adolf <stadolf@gmail.com>

--- contracts/truffle-config.js
+++ contracts/truffle-config.js
@@ -56,35 +56,9 @@ module.exports = {
     //   network_id: "*",       // Any network (default: none)
     //   provider: () => new HDWalletProvider(mnemonic, `ws://127.0.0.1:7545`),
     // },
-    // Another network with more advanced options...
-    // advanced: {
-    // port: 8777,             // Custom port
-    // network_id: 1342,       // Custom network
-    // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-    // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-    // from: <address>,        // Account to send txs from (default: accounts[0])
-    // websocket: true        // Enable EventEmitter interface for web3 (default: false)
-    // },
-    // Useful for deploying to a public network.
-    // NB: It's important to wrap the provider as a function.
-    kovan: {
-      provider: () => new HDWalletProvider({
-        privateKeys: process.env.DEPLOYER_PRIVATEKEY.split(","),
-        url: `https://kovan.infura.io/v3/${process.env.INFURA_KEY}`
-      }),
-
-      network_id: 42,       // Ropsten's id
-      gas: 5000000,        // Ropsten has a lower block limit than mainnet
-      confirmations: 1,    // # of confs to wait between deployments. (default: 0)
-      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: false     // Skip dry run before migrations? (default: false for public nets )
-    },
-    // Useful for private networks
-    // private: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-    // network_id: 2111,   // This network is yours, in the cloud.
-    // production: true    // Treats this network as if it was a public net. (default: false)
-    // }
+    dashboard: {
+      networkCheckTimeout: 120000,
+    }
   },
 
   // Set default mocha options here, use special reporters etc.
